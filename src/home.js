86d378bf7a5ca2dbb6302768e49354c02b7c4744
Brent Cameron <bjcameron@gmail.com>
2022-01-04 12:11:48 -0700

    added order buttons/styles

--- src/home.js
+++ src/home.js
@@ -16,11 +16,24 @@ export default function home(){
   let home = document.createElement('div');
   home.classList.add('container');
   // test.innerHTML = "hello I am from module: home";
+  let order = document.createElement('div');
+  order.classList.add('order');
+  let delivery_btn = document.createElement('button');
+  delivery_btn.innerHTML = "Order Delivery!";
+  let pickup_btn = document.createElement('button');
+
+  pickup_btn.innerHTML = "Order Pickup!";
+  order.appendChild(delivery_btn);
+  order.appendChild(pickup_btn);
+  home.appendChild(order);
+
   let test_content = document.createTextNode('Hello I am from module: home ');
   home.appendChild(test_content);
   let test = document.createElement('div');
   test.innerHTML = 'Hello inside';
-  test.classList.add('content')
+  test.classList.add('content');
+  test.classList.add('container');
+
   home.appendChild(test);
   return home;
 }
