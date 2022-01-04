da87657d26e02c9381960d8c7948af647594f379
Brent Cameron <bjcameron@gmail.com>
2022-01-04 12:23:02 -0700

    added title and stock image

--- src/home.js
+++ src/home.js
@@ -6,29 +6,38 @@
 //   return test;
 // }
 
+
 // export default home;
 
 // export default function home(){
 //   console.log('i get called from home');
 // }
+import pizza from './Assets/images/pizza1.jpg';
 
 export default function home(){
   let home = document.createElement('div');
   home.classList.add('container');
-  // test.innerHTML = "hello I am from module: home";
+
+  // order div 
   let order = document.createElement('div');
   order.classList.add('order');
   let delivery_btn = document.createElement('button');
   delivery_btn.innerHTML = "Order Delivery!";
   let pickup_btn = document.createElement('button');
-
   pickup_btn.innerHTML = "Order Pickup!";
   order.appendChild(delivery_btn);
   order.appendChild(pickup_btn);
   home.appendChild(order);
 
-  let test_content = document.createTextNode('Hello I am from module: home ');
-  home.appendChild(test_content);
+  let title = document.createElement('h1');
+  title.innerHTML = "Welcome to PizzaPalace!"
+  home.appendChild(title);
+  // let photo = document.createElement('img');
+  const photo = new Image();
+  photo.src = pizza;
+
+  home.appendChild(photo);
+
   let test = document.createElement('div');
   test.innerHTML = 'Hello inside';
   test.classList.add('content');
