26c8fc13d75c981adba88b3026d2248c7c747b8c
Brent Cameron <bjcameron@gmail.com>
2022-01-05 17:29:51 -0700

    filled out a simple contact page

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,17 @@
 export default function contact(){
   let contact = document.createElement('div');
   contact.classList.add('container');
-  contact.innerHTML = 'Hi this is cool contact';
+  contact.classList.add('hot_pizza');
+
+  let title = document.createElement('h1');
+  title.innerHTML = "Contact Page";
+  title.style.color = 'white';
+  contact.appendChild(title);
+
+  let card = document.createElement('div');
+  card.classList.add('contact_card');
+  card.innerHTML = 'Thanks for visiting my fake restaurant!';
+  contact.appendChild(card);
 
   return contact;
 }
